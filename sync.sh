397d0dc875c93039cedf76ccf75e7bad386b7710
jm33-m0 <jm33@jm33.me>
2022-02-10 17:13:15 +0800

    auto push

--- sync.sh
+++ sync.sh
@@ -9,3 +9,10 @@ wget -O accelerated-domains.china.conf "$upstream_url" || {
 
 echo "*.cn" >china-list.txt
 awk -F '/' '{print "*."$2}' <accelerated-domains.china.conf >>china-list.txt
+
+# commit
+git config user.name "github-actions"
+git config user.email "github-actions@users.noreply.github.com"
+git add .
+git commit -a -m "Updated by Github Actions"
+git push
