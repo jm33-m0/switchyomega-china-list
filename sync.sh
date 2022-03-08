4fddfd0ed2af8849ba4395d1014784b61298e36c
jm33-m0 <jm33@jm33.me>
2022-03-08 11:12:10 +0800

    dnspod DoH server

--- sync.sh
+++ sync.sh
@@ -18,156 +18,157 @@ awk -F '/' '{print "*."$2}' <accelerated-domains.china.conf >>china-list.txt
 echo '
 # upstream servers
 10.2.3.1
-https://9.9.9.9/dns-query
+https://1.1.1.1/dns-query
+https://8.8.8.8/dns-query
 
 # jm33.me
-[/jm33.me/]https://9.9.9.9/dns-query
+[/jm33.me/]https://1.1.1.1/dns-query
 
 # connectivity test
-[/msftconnecttest.com/]114.114.114.114
-[/msftncsi.com/]114.114.114.114
-# [/googleapis.com/]114.114.114.114
-# [/gstatic.com/]114.114.114.114
-# [/google.cn/]114.114.114.114
-# [/googleapis.cn/]114.114.114.114
+[/msftconnecttest.com/]https://1.12.12.12/dns-query
+[/msftncsi.com/]https://1.12.12.12/dns-query
+# [/googleapis.com/]https://1.12.12.12/dns-query
+# [/gstatic.com/]https://1.12.12.12/dns-query
+# [/google.cn/]https://1.12.12.12/dns-query
+# [/googleapis.cn/]https://1.12.12.12/dns-query
 
 # CN domains
-[/cn/]114.114.114.114
+[/cn/]https://1.12.12.12/dns-query
 
 # my domains
 [/jm33.local/]127.0.0.1
 
 # Apple domains
-[/a1.mzstatic.com/]114.114.114.114
-[/a2.mzstatic.com/]114.114.114.114
-[/a3.mzstatic.com/]114.114.114.114
-[/a4.mzstatic.com/]114.114.114.114
-[/a5.mzstatic.com/]114.114.114.114
-[/adcdownload.apple.com.akadns.net/]114.114.114.114
-[/adcdownload.apple.com/]114.114.114.114
-[/appldnld.apple.com/]114.114.114.114
-[/appldnld.g.aaplimg.com/]114.114.114.114
-[/appleid.cdn-apple.com/]114.114.114.114
-[/apps.apple.com/]114.114.114.114
-[/apps.mzstatic.com/]114.114.114.114
-[/cdn-cn1.apple-mapkit.com/]114.114.114.114
-[/cdn-cn2.apple-mapkit.com/]114.114.114.114
-[/cdn-cn3.apple-mapkit.com/]114.114.114.114
-[/cdn-cn4.apple-mapkit.com/]114.114.114.114
-[/cdn.apple-mapkit.com/]114.114.114.114
-[/cdn1.apple-mapkit.com/]114.114.114.114
-[/cdn2.apple-mapkit.com/]114.114.114.114
-[/cdn3.apple-mapkit.com/]114.114.114.114
-[/cdn4.apple-mapkit.com/]114.114.114.114
-[/cds-cdn.v.aaplimg.com/]114.114.114.114
-[/cds.apple.com.akadns.net/]114.114.114.114
-[/cds.apple.com/]114.114.114.114
-[/cl1-cdn.origin-apple.com.akadns.net/]114.114.114.114
-[/cl1.apple.com/]114.114.114.114
-[/cl2-cn.apple.com/]114.114.114.114
-[/cl2.apple.com.edgekey.net.globalredir.akadns.net/]114.114.114.114
-[/cl2.apple.com/]114.114.114.114
-[/cl3-cdn.origin-apple.com.akadns.net/]114.114.114.114
-[/cl3.apple.com/]114.114.114.114
-[/cl4-cdn.origin-apple.com.akadns.net/]114.114.114.114
-[/cl4-cn.apple.com/]114.114.114.114
-[/cl4.apple.com/]114.114.114.114
-[/cl5-cdn.origin-apple.com.akadns.net/]114.114.114.114
-[/cl5.apple.com/]114.114.114.114
-[/clientflow.apple.com.akadns.net/]114.114.114.114
-[/clientflow.apple.com/]114.114.114.114
-[/configuration.apple.com.akadns.net/]114.114.114.114
-[/configuration.apple.com/]114.114.114.114
-[/cstat.apple.com/]114.114.114.114
-[/dd-cdn.origin-apple.com.akadns.net/]114.114.114.114
-[/download.developer.apple.com/]114.114.114.114
-[/gs-loc-cn.apple.com/]114.114.114.114
-[/gs-loc.apple.com/]114.114.114.114
-[/gsp10-ssl-cn.ls.apple.com/]114.114.114.114
-[/gsp11-cn.ls.apple.com/]114.114.114.114
-[/gsp12-cn.ls.apple.com/]114.114.114.114
-[/gsp13-cn.ls.apple.com/]114.114.114.114
-[/gsp4-cn.ls.apple.com.edgekey.net.globalredir.akadns.net/]114.114.114.114
-[/gsp4-cn.ls.apple.com.edgekey.net/]114.114.114.114
-[/gsp4-cn.ls.apple.com/]114.114.114.114
-[/gsp5-cn.ls.apple.com/]114.114.114.114
-[/gsp85-cn-ssl.ls.apple.com/]114.114.114.114
-[/gspe19-cn-ssl.ls.apple.com/]114.114.114.114
-[/gspe19-cn.ls-apple.com.akadns.net/]114.114.114.114
-[/gspe19-cn.ls.apple.com/]114.114.114.114
-[/gspe21-ssl.ls.apple.com/]114.114.114.114
-[/gspe21.ls.apple.com/]114.114.114.114
-[/gspe35-ssl.ls.apple.com/]114.114.114.114
-[/iadsdk.apple.com/]114.114.114.114
-[/icloud-cdn.icloud.com.akadns.net/]114.114.114.114
-[/icloud.cdn-apple.com/]114.114.114.114
-[/images.apple.com.akadns.net/]114.114.114.114
-[/images.apple.com.edgekey.net.globalredir.akadns.net/]114.114.114.114
-[/images.apple.com/]114.114.114.114
-[/init-p01md-lb.push-apple.com.akadns.net/]114.114.114.114
-[/init-p01md.apple.com/]114.114.114.114
-[/init-p01st-lb.push-apple.com.akadns.net/]114.114.114.114
-[/init-p01st.push.apple.com/]114.114.114.114
-[/init-s01st-lb.push-apple.com.akadns.net/]114.114.114.114
-[/init-s01st.push.apple.com/]114.114.114.114
-[/iosapps.itunes.g.aaplimg.com/]114.114.114.114
-[/iphone-ld.apple.com/]114.114.114.114
-[/is1-ssl.mzstatic.com/]114.114.114.114
-[/is1.mzstatic.com/]114.114.114.114
-[/is2-ssl.mzstatic.com/]114.114.114.114
-[/is2.mzstatic.com/]114.114.114.114
-[/is3-ssl.mzstatic.com/]114.114.114.114
-[/is3.mzstatic.com/]114.114.114.114
-[/is4-ssl.mzstatic.com/]114.114.114.114
-[/is4.mzstatic.com/]114.114.114.114
-[/is5-ssl.mzstatic.com/]114.114.114.114
-[/is5.mzstatic.com/]114.114.114.114
-[/itunes-apple.com.akadns.net/]114.114.114.114
-[/itunes.apple.com/]114.114.114.114
-[/itunesconnect.apple.com/]114.114.114.114
-[/mesu-cdn.apple.com.akadns.net/]114.114.114.114
-[/mesu-china.apple.com.akadns.net/]114.114.114.114
-[/mesu.apple.com/]114.114.114.114
-[/music.apple.com/]114.114.114.114
-[/ocsp-lb.apple.com.akadns.net/]114.114.114.114
-[/ocsp.apple.com/]114.114.114.114
-[/oscdn.apple.com/]114.114.114.114
-[/oscdn.origin-apple.com.akadns.net/]114.114.114.114
-[/pancake.apple.com/]114.114.114.114
-[/pancake.cdn-apple.com.akadns.net/]114.114.114.114
-[/phobos.apple.com/]114.114.114.114
-[/prod-support.apple-support.akadns.net/]114.114.114.114
-[/reserve-prime.apple.com/]114.114.114.114
-[/s.mzstatic.com/]114.114.114.114
-[/stocks-sparkline-lb.apple.com.akadns.net/]114.114.114.114
-[/store.apple.com.edgekey.net.globalredir.akadns.net/]114.114.114.114
-[/store.apple.com.edgekey.net/]114.114.114.114
-[/store.apple.com/]114.114.114.114
-[/store.storeimages.apple.com.akadns.net/]114.114.114.114
-[/store.storeimages.cdn-apple.com/]114.114.114.114
-[/support-china.apple-support.akadns.net/]114.114.114.114
-[/support.apple.com/]114.114.114.114
-[/swcatalog-cdn.apple.com.akadns.net/]114.114.114.114
-[/swcatalog.apple.com/]114.114.114.114
-[/swcdn.apple.com/]114.114.114.114
-[/swcdn.g.aaplimg.com/]114.114.114.114
-[/swdist.apple.com.akadns.net/]114.114.114.114
-[/swdist.apple.com/]114.114.114.114
-[/swscan-cdn.apple.com.akadns.net/]114.114.114.114
-[/swscan.apple.com/]114.114.114.114
-[/updates-http.cdn-apple.com.akadns.net/]114.114.114.114
-[/updates-http.cdn-apple.com/]114.114.114.114
-[/updates.cdn-apple.com/]114.114.114.114
-[/valid.apple.com/]114.114.114.114
-[/valid.origin-apple.com.akadns.net/]114.114.114.114
-[/www.apple.com.edgekey.net.globalredir.akadns.net/]114.114.114.114
-[/www.apple.com.edgekey.net/]114.114.114.114
-[/www.apple.com/]114.114.114.114
+[/a1.mzstatic.com/]https://1.12.12.12/dns-query
+[/a2.mzstatic.com/]https://1.12.12.12/dns-query
+[/a3.mzstatic.com/]https://1.12.12.12/dns-query
+[/a4.mzstatic.com/]https://1.12.12.12/dns-query
+[/a5.mzstatic.com/]https://1.12.12.12/dns-query
+[/adcdownload.apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/adcdownload.apple.com/]https://1.12.12.12/dns-query
+[/appldnld.apple.com/]https://1.12.12.12/dns-query
+[/appldnld.g.aaplimg.com/]https://1.12.12.12/dns-query
+[/appleid.cdn-apple.com/]https://1.12.12.12/dns-query
+[/apps.apple.com/]https://1.12.12.12/dns-query
+[/apps.mzstatic.com/]https://1.12.12.12/dns-query
+[/cdn-cn1.apple-mapkit.com/]https://1.12.12.12/dns-query
+[/cdn-cn2.apple-mapkit.com/]https://1.12.12.12/dns-query
+[/cdn-cn3.apple-mapkit.com/]https://1.12.12.12/dns-query
+[/cdn-cn4.apple-mapkit.com/]https://1.12.12.12/dns-query
+[/cdn.apple-mapkit.com/]https://1.12.12.12/dns-query
+[/cdn1.apple-mapkit.com/]https://1.12.12.12/dns-query
+[/cdn2.apple-mapkit.com/]https://1.12.12.12/dns-query
+[/cdn3.apple-mapkit.com/]https://1.12.12.12/dns-query
+[/cdn4.apple-mapkit.com/]https://1.12.12.12/dns-query
+[/cds-cdn.v.aaplimg.com/]https://1.12.12.12/dns-query
+[/cds.apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/cds.apple.com/]https://1.12.12.12/dns-query
+[/cl1-cdn.origin-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/cl1.apple.com/]https://1.12.12.12/dns-query
+[/cl2-cn.apple.com/]https://1.12.12.12/dns-query
+[/cl2.apple.com.edgekey.net.globalredir.akadns.net/]https://1.12.12.12/dns-query
+[/cl2.apple.com/]https://1.12.12.12/dns-query
+[/cl3-cdn.origin-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/cl3.apple.com/]https://1.12.12.12/dns-query
+[/cl4-cdn.origin-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/cl4-cn.apple.com/]https://1.12.12.12/dns-query
+[/cl4.apple.com/]https://1.12.12.12/dns-query
+[/cl5-cdn.origin-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/cl5.apple.com/]https://1.12.12.12/dns-query
+[/clientflow.apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/clientflow.apple.com/]https://1.12.12.12/dns-query
+[/configuration.apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/configuration.apple.com/]https://1.12.12.12/dns-query
+[/cstat.apple.com/]https://1.12.12.12/dns-query
+[/dd-cdn.origin-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/download.developer.apple.com/]https://1.12.12.12/dns-query
+[/gs-loc-cn.apple.com/]https://1.12.12.12/dns-query
+[/gs-loc.apple.com/]https://1.12.12.12/dns-query
+[/gsp10-ssl-cn.ls.apple.com/]https://1.12.12.12/dns-query
+[/gsp11-cn.ls.apple.com/]https://1.12.12.12/dns-query
+[/gsp12-cn.ls.apple.com/]https://1.12.12.12/dns-query
+[/gsp13-cn.ls.apple.com/]https://1.12.12.12/dns-query
+[/gsp4-cn.ls.apple.com.edgekey.net.globalredir.akadns.net/]https://1.12.12.12/dns-query
+[/gsp4-cn.ls.apple.com.edgekey.net/]https://1.12.12.12/dns-query
+[/gsp4-cn.ls.apple.com/]https://1.12.12.12/dns-query
+[/gsp5-cn.ls.apple.com/]https://1.12.12.12/dns-query
+[/gsp85-cn-ssl.ls.apple.com/]https://1.12.12.12/dns-query
+[/gspe19-cn-ssl.ls.apple.com/]https://1.12.12.12/dns-query
+[/gspe19-cn.ls-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/gspe19-cn.ls.apple.com/]https://1.12.12.12/dns-query
+[/gspe21-ssl.ls.apple.com/]https://1.12.12.12/dns-query
+[/gspe21.ls.apple.com/]https://1.12.12.12/dns-query
+[/gspe35-ssl.ls.apple.com/]https://1.12.12.12/dns-query
+[/iadsdk.apple.com/]https://1.12.12.12/dns-query
+[/icloud-cdn.icloud.com.akadns.net/]https://1.12.12.12/dns-query
+[/icloud.cdn-apple.com/]https://1.12.12.12/dns-query
+[/images.apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/images.apple.com.edgekey.net.globalredir.akadns.net/]https://1.12.12.12/dns-query
+[/images.apple.com/]https://1.12.12.12/dns-query
+[/init-p01md-lb.push-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/init-p01md.apple.com/]https://1.12.12.12/dns-query
+[/init-p01st-lb.push-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/init-p01st.push.apple.com/]https://1.12.12.12/dns-query
+[/init-s01st-lb.push-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/init-s01st.push.apple.com/]https://1.12.12.12/dns-query
+[/iosapps.itunes.g.aaplimg.com/]https://1.12.12.12/dns-query
+[/iphone-ld.apple.com/]https://1.12.12.12/dns-query
+[/is1-ssl.mzstatic.com/]https://1.12.12.12/dns-query
+[/is1.mzstatic.com/]https://1.12.12.12/dns-query
+[/is2-ssl.mzstatic.com/]https://1.12.12.12/dns-query
+[/is2.mzstatic.com/]https://1.12.12.12/dns-query
+[/is3-ssl.mzstatic.com/]https://1.12.12.12/dns-query
+[/is3.mzstatic.com/]https://1.12.12.12/dns-query
+[/is4-ssl.mzstatic.com/]https://1.12.12.12/dns-query
+[/is4.mzstatic.com/]https://1.12.12.12/dns-query
+[/is5-ssl.mzstatic.com/]https://1.12.12.12/dns-query
+[/is5.mzstatic.com/]https://1.12.12.12/dns-query
+[/itunes-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/itunes.apple.com/]https://1.12.12.12/dns-query
+[/itunesconnect.apple.com/]https://1.12.12.12/dns-query
+[/mesu-cdn.apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/mesu-china.apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/mesu.apple.com/]https://1.12.12.12/dns-query
+[/music.apple.com/]https://1.12.12.12/dns-query
+[/ocsp-lb.apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/ocsp.apple.com/]https://1.12.12.12/dns-query
+[/oscdn.apple.com/]https://1.12.12.12/dns-query
+[/oscdn.origin-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/pancake.apple.com/]https://1.12.12.12/dns-query
+[/pancake.cdn-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/phobos.apple.com/]https://1.12.12.12/dns-query
+[/prod-support.apple-support.akadns.net/]https://1.12.12.12/dns-query
+[/reserve-prime.apple.com/]https://1.12.12.12/dns-query
+[/s.mzstatic.com/]https://1.12.12.12/dns-query
+[/stocks-sparkline-lb.apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/store.apple.com.edgekey.net.globalredir.akadns.net/]https://1.12.12.12/dns-query
+[/store.apple.com.edgekey.net/]https://1.12.12.12/dns-query
+[/store.apple.com/]https://1.12.12.12/dns-query
+[/store.storeimages.apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/store.storeimages.cdn-apple.com/]https://1.12.12.12/dns-query
+[/support-china.apple-support.akadns.net/]https://1.12.12.12/dns-query
+[/support.apple.com/]https://1.12.12.12/dns-query
+[/swcatalog-cdn.apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/swcatalog.apple.com/]https://1.12.12.12/dns-query
+[/swcdn.apple.com/]https://1.12.12.12/dns-query
+[/swcdn.g.aaplimg.com/]https://1.12.12.12/dns-query
+[/swdist.apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/swdist.apple.com/]https://1.12.12.12/dns-query
+[/swscan-cdn.apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/swscan.apple.com/]https://1.12.12.12/dns-query
+[/updates-http.cdn-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/updates-http.cdn-apple.com/]https://1.12.12.12/dns-query
+[/updates.cdn-apple.com/]https://1.12.12.12/dns-query
+[/valid.apple.com/]https://1.12.12.12/dns-query
+[/valid.origin-apple.com.akadns.net/]https://1.12.12.12/dns-query
+[/www.apple.com.edgekey.net.globalredir.akadns.net/]https://1.12.12.12/dns-query
+[/www.apple.com.edgekey.net/]https://1.12.12.12/dns-query
+[/www.apple.com/]https://1.12.12.12/dns-query
 
 # Chinese domains
 ' >upstream-dns.txt
 
-awk -F '/' '{print "[/"$2"/]114.114.114.114"}' <accelerated-domains.china.conf >>upstream-dns.txt
+awk -F '/' '{print "[/"$2"/https:\/\/1.12.12.12\/dns-query]"}' <accelerated-domains.china.conf >>upstream-dns.txt
 
 # commit
 git config user.name "github-actions"
